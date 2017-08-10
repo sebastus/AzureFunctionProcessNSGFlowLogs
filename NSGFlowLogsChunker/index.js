f85f16a511a1ffc28c29c99645cf82d718e1d9f0
Greg Oliver <divingclimber@gmail.com>
2017-08-10 13:14:25 -0700

    add code to read blob

--- NSGFlowLogsChunker/index.js
+++ NSGFlowLogsChunker/index.js
@@ -1,16 +1,43 @@
 //var _ = require('underscore');
-//var azure = require('azure-storage');
+var azure = require('azure-storage');
 
 module.exports = function(context) {
-    // Additional inputs can be accessed by the arguments property
-    // if(arguments.length === 4) {
-    //     context.log('This function has 4 inputs');
-    // }
-    var trigger = context.bindings.inputBlock;
 
-    context.log('BlobName is: ' + trigger.BlobName);
-    context.log('Start is: ' + trigger.Start);
+    var trigger = context.bindings.inputBlock;
+    var blobName = trigger.BlobName;
+    var start = trigger.Start;
+    var end = trigger.Length + start - 1;
+
+    context.log('BlobName is: ' + blobName);
+    context.log('Start is: ' + start);
     context.log('Length is: ' + trigger.Length);
 
-    context.done();
+    var nsgSourceDataAccount = process.env['nsgSourceDataAccount'];
+    var connStr = process.env[nsgSourceDataAccount];
+
+    var blobService = azure.createBlobService(connStr).withFilter(new azure.ExponentialRetryPolicyFilter());
+
+    var containerName = process.env['blobContainerName'];
+
+    blobService.getBlobToText(containerName, blobName, {rangeStart:start, rangeEnd: end}, function (downloadErr, blobText, blob, downloadResponse) {
+
+        if (downloadErr !== null) {
+
+            console.log("error reading blob");
+
+        } else {
+
+            var curlyBrace = blobText.indexOf('{');
+            if (curlyBrace !== 0) {
+                blobText = blobText.substr(curlyBrace);
+            }
+
+            var records = '{"records":[' + blobText + ']}';
+            var recordsJson = JSON.parse(records);
+
+
+            context.done();
+        }
+    });
+
 };
