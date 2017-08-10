c4812947ec31c3629d86068126971b5717ac9e45
Greg Oliver <divingclimber@gmail.com>
2017-08-10 15:41:19 -0700

    comment out logs

--- NSGFlowLogsChunker/index.js
+++ NSGFlowLogsChunker/index.js
@@ -13,20 +13,20 @@ module.exports = function(context) {
     var start = trigger.Start;
     var end = trigger.Length + start - 1;
 
-    context.log('BlobName is: ' + blobName);
-    context.log('Start is: ' + start);
-    context.log('Length is: ' + trigger.Length);
+    //context.log('BlobName is: ' + blobName);
+    //context.log('Start is: ' + start);
+    //context.log('Length is: ' + trigger.Length);
 
     var nsgSourceDataAccount = process.env.nsgSourceDataAccount;
-    context.log('Data account is: ' + nsgSourceDataAccount);
+    //context.log('Data account is: ' + nsgSourceDataAccount);
 
     var connStr = process.env[nsgSourceDataAccount];
-    context.log('Connection string is: ' + connStr);
+    //context.log('Connection string is: ' + connStr);
 
     var blobService = azure.createBlobService(connStr).withFilter(new azure.ExponentialRetryPolicyFilter());
 
     var containerName = process.env.blobContainerName;
-    context.log('containerName is: ' + containerName);
+    //context.log('containerName is: ' + containerName);
 
     blobService.getBlobToText(containerName, blobName, {rangeStart:start, rangeEnd: end}, function (downloadErr, blobText, blob, downloadResponse) {
 
@@ -36,8 +36,6 @@ module.exports = function(context) {
 
         } else {
 
-            context.log("read the blob");
-
             var curlyBrace = blobText.indexOf('{');
             if (curlyBrace !== 0) {
                 blobText = blobText.substr(curlyBrace);
@@ -45,7 +43,7 @@ module.exports = function(context) {
 
             var records = '{"records":[' + blobText + ']}';
             var recordsJson = JSON.parse(records);
-            context.log('# of records is: ' + recordsJson.records.length);
+            //context.log('# of records is: ' + recordsJson.records.length);
 
             var outputQueueMessages = [];
             outputQueueMessages.push({BlobName: blobName, Start: start, Length: 0});
@@ -54,7 +52,7 @@ module.exports = function(context) {
             _.each(recordsJson.records, function(record) {
 
                 recordLength = JSON.stringify(record).length;
-                context.log('recordLength is: ' + recordLength);
+                //context.log('recordLength is: ' + recordLength);
 
                 if (recordLength + outputQueueMessages[currentChunk].Length > MAX_CHUNK_SIZE) {
 
