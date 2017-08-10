e37915477cfa1e853451f9bc219161d9f590c411
Greg Oliver <divingclimber@gmail.com>
2017-08-10 12:24:44 -0700

    add block chunker function

---
+++ NSGFlowLogsChunker/index.js
@@ -0,0 +1,16 @@
+var _ = require('underscore');
+var azure = require('azure-storage');
+
+module.exports = function(context) {
+    // Additional inputs can be accessed by the arguments property
+    // if(arguments.length === 4) {
+    //     context.log('This function has 4 inputs');
+    // }
+    var trigger = context.bindings.inputBlock;
+
+    context.log('BlobName is: ' + trigger.BlobName);
+    context.log('Start is: ' + trigger.Start);
+    context.log('Length is: ' + trigger.Length);
+
+    context.done();
+};
